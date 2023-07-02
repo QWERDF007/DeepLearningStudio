7a1b5e90d1cc522d570465b306b4da80127254d3
wangtao <595517713@qq.com>
2023-07-02 16:03:20 +0800

    设置Qt寻找路径

--- cmake/ConfigQT.cmake
+++ cmake/ConfigQT.cmake
@@ -1,5 +1,9 @@
+# 设置 Qt 的 cmake 文件目录
+set(QT_CMAKE_DIR "H:/Software/Qt/6.5.0/msvc2019_64/lib/cmake/Qt6")
+
+
 # 寻找Qt6的Quick组件
-find_package(Qt6 6.5 REQUIRED COMPONENTS Core Gui Quick PATHS "H:/Software/Qt/6.5.0/msvc2019_64/lib/cmake/Qt6")
+find_package(Qt6 6.5 REQUIRED COMPONENTS Core Gui Quick PATHS ${QT_CMAKE_DIR})
 
 # 设置Qt6项目
 qt_standard_project_setup(REQUIRES 6.5)
