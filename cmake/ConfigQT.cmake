abd188d2e8f861b7c8c94f8a662e46005b84db47
wangtao <595517713@qq.com>
2023-07-09 20:35:44 +0800

    Qt更新为6.5.1，寻找测试组件

--- cmake/ConfigQT.cmake
+++ cmake/ConfigQT.cmake
@@ -1,9 +1,12 @@
 # 设置 Qt 的 cmake 文件目录
-set(QT_CMAKE_DIR "H:/Software/Qt/6.5.0/msvc2019_64/lib/cmake/Qt6")
+set(QT_CMAKE_DIR "H:/Software/Qt/6.5.1/msvc2019_64/lib/cmake/Qt6")
 
 
 # 寻找Qt6的Quick组件
 find_package(Qt6 6.5 REQUIRED COMPONENTS Core Gui Quick PATHS ${QT_CMAKE_DIR})
+
+# 寻找Qt6的测试组件
+find_package(Qt6 6.5 REQUIRED COMPONENTS QuickTest)
 
 # 设置Qt6项目
 qt_standard_project_setup(REQUIRES 6.5)
